afcfc0a0f5b24ea6ad82c68fa2b3db3217653cc9
Junior Developer <lotifurshabbir54@gmail.com>
2023-01-30 11:54:55 +0600

    reverse a stack using another stack

--- Module practice-phitron/M 21/4.cpp
+++ Module practice-phitron/M 21/4.cpp
@@ -1,87 +1,144 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+template<class T>
+class node
+{
+public:
+    T data;
+    node *nxt;
+    node *prv;
 
-//stack using dynamic array
+};
+template<class T>
+class DoublyLinkedList
+{
+public:
+    node<T> *head;//important line
+    int sz;
+    DoublyLinkedList()//constructor
+    {
+        head=NULL;
+        sz=0;
+    }
+    //create a new node with the given data and returns it.
+    node<T> *CreateNewNode(T data)
+    {
+        node<T> *newnode = new node<T>;
+        newnode->data = data;
+        newnode->nxt = NULL;
+        newnode->prv = NULL;
+        return newnode;
+    }
+
+    //inserting value at Head
+    void InsertAtHead(T data)//TC=O(1)
+    {
+        sz++;
+        node<T> *newnode = CreateNewNode(data);
+        if(head == NULL)
+        {
+            head = newnode;
+            return;
+        }
+        node<T> *a = head;//assigning head in variable a;
+        newnode->nxt = a;
+        a->prv = newnode;
+        head = newnode;
+    }
+
+    //delete from head
+    void DeleteAtHead()
+    {
+        if(head==NULL)
+        {
+            return;
+        }
+        node<T> *a = head;
+        node<T> *b = head->nxt;
+        delete a;
+        if(b!=NULL)
+            b->prv = NULL;
+        head = b;
+        sz--;
+    }
+
+    //returns the size of LinkedList
+    int getSize()//TC=O(1)
+    {
+        return sz;
+    }
+};
+
+//Stack using doubly linked list
+template<class T>
 class Stack
 {
 public:
+    DoublyLinkedList<T> dl;
 
-    int *a;
-    int stack_size;
-    int cap;
-    Stack()//constructor
+    Stack()
     {
-        a = new int[1];
-        cap = 1;
-        stack_size = 0;
+
     }
 
-    //Makes the array cap * 2
-    void increase_size()
+    T top()
     {
-        int *tmp;
-        tmp = new int[cap*2];
-        for(int i=0;i<cap;i++)
-            tmp[i] = a[i];
-        swap(a,tmp);
-        delete []tmp;//deleting temp array
-        cap = cap*2;//increase two times bigger than before
-    }
-
-    //add an element in the stack. O(1)
-    void push(int val)
-    {
-        if(stack_size+1 > cap)
+        if(dl.getSize()==0)
         {
-            increase_size();
+            cout<<"Stack faka vai!\n";
+            T a;
+            return a;
         }
-
-        stack_size+=1;
-        a[stack_size - 1] = val;
-        //last e dhap kore value ta bosai dilam
+        return dl.head->data;
     }
 
-    //Delete the top most element from the stack. O(1)
+    void push(T val)
+    {
+        dl.InsertAtHead(val);
+    }
+
     void pop()
     {
-        if(stack_size == 0)
+        if(dl.getSize()==0)
         {
-            cout<<"Stack is empty.\n";
+            cout<<"Stack e kicu nai vai!\n";
             return;
         }
-        a[stack_size - 1] = 0;
-        stack_size-=1;
+        dl.DeleteAtHead();
     }
 
-    //return the top element form the stack
-    int top()
+    int getSize()
     {
-        if(stack_size == 0)
-        {
-            cout<<"Stack is empty.\n";
-            return -1;
-        }
-        return a[stack_size-1];
+        return dl.getSize();
     }
 };
 
 int main()
 {
-    Stack st;
-    st.push(3);
-    cout<<st.top()<<endl;
-    st.push(4);
-    cout<<st.top()<<endl;
-    st.push(5);
-    cout<<st.top()<<endl;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    st.pop();
-    cout<<st.top()<<endl;
-    st.pop();
-    cout<<st.top()<<endl;
-    st.pop();
-    cout<<st.top()<<endl;
+    //reverse a using another stack
+    Stack<int>a;
+
+    a.push(4);
+    a.push(6);
+    a.push(3);
+    a.push(7);
+
+    Stack<int>tmp;
+
+    while(a.getSize()>0)
+    {
+        tmp.push(a.top());
+        a.pop();
+    }
+
+    swap(a,tmp);
+
+
 
     return 0;
 }
