958ccb10e109cb55b37b9c35c4f6a21d5b840019
Junior Developer <lotifurshabbir54@gmail.com>
2023-01-30 11:25:20 +0600

    template based stack part-2

--- Module practice-phitron/M 21/3.cpp
+++ Module practice-phitron/M 21/3.cpp
@@ -1,73 +1,154 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int MAX_SIZE = 500;
 
-//stack using static array
+template<class T>
+class node
+{
+public:
+    T data;
+    node *nxt;
+    node *prv;
+
+};
+template<class T>
+class DoublyLinkedList
+{
+public:
+    node<T> *head;//important line
+    int sz;
+    DoublyLinkedList()//constructor
+    {
+        head=NULL;
+        sz=0;
+    }
+    //create a new node with the given data and returns it.
+    node<T> *CreateNewNode(T data)
+    {
+        node<T> *newnode = new node<T>;
+        newnode->data = data;
+        newnode->nxt = NULL;
+        newnode->prv = NULL;
+        return newnode;
+    }
+
+    //inserting value at Head
+    void InsertAtHead(T data)//TC=O(1)
+    {
+        sz++;
+        node<T> *newnode = CreateNewNode(data);
+        if(head == NULL)
+        {
+            head = newnode;
+            return;
+        }
+        node<T> *a = head;//assigning head in variable a;
+        newnode->nxt = a;
+        a->prv = newnode;
+        head = newnode;
+    }
+
+    //delete from head
+    void DeleteAtHead()
+    {
+        if(head==NULL)
+        {
+            return;
+        }
+        node<T> *a = head;
+        node<T> *b = head->nxt;
+        delete a;
+        if(b!=NULL)
+            b->prv = NULL;
+        head = b;
+        sz--;
+    }
+
+    //returns the size of LinkedList
+    int getSize()//TC=O(1)
+    {
+        return sz;
+    }
+};
+
+//Stack using doubly linked list
+template<class T>
 class Stack
 {
 public:
+    DoublyLinkedList<T> dl;
 
-    int a[MAX_SIZE];
-    int stack_size;
-    Stack()//constructor
+    Stack()
     {
-        stack_size = 0;
+
     }
 
-    //add an element in the stack. O(1)
-    void push(int val)
+    T top()
     {
-        if(stack_size+1 > MAX_SIZE)
+        if(dl.getSize()==0)
         {
-            cout<<"Stack is full!\n";
-            return;
+            cout<<"Stack faka vai!\n";
+            T a;
+            return a;
         }
-
-        stack_size+=1;
-        a[stack_size - 1] = val;
-        //last e dhap kore value ta bosai dilam
+        return dl.head->data;
     }
 
-    //Delete the top most element from the stack. O(1)
+    void push(T val)
+    {
+        dl.InsertAtHead(val);
+    }
+
     void pop()
     {
-        if(stack_size == 0)
+        if(dl.getSize()==0)
         {
-            cout<<"Stack is empty.\n";
+            cout<<"Stack e kicu nai vai!\n";
             return;
         }
-        a[stack_size - 1] = 0;
-        stack_size-=1;
-    }
-
-    //return the top element form the stack
-    int top()
-    {
-        if(stack_size == 0)
-        {
-            cout<<"Stack is empty.\n";
-            return -1;
-        }
-        return a[stack_size-1];
+        dl.DeleteAtHead();
     }
 };
 
 int main()
 {
-    Stack st;
-    st.push(3);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+//    DoublyLinkedList <char> dl;
+//    dl.InsertAtHead('c');
+//    dl.InsertAtHead('cs');
+//    dl.InsertAtHead('cse');
+//
+//    dl.DeleteAtHead();
+//    dl.DeleteAtHead();
+
+    Stack<double>st;
+    st.push(3.5);
+    st.push(3.6);
+    st.push(7.8);
     cout<<st.top()<<endl;
-    st.push(4);
+    st.pop();
     cout<<st.top()<<endl;
-    st.push(5);
+    st.pop();
+    cout<<st.top()<<endl;
+    st.pop();
     cout<<st.top()<<endl;
 
-    st.pop();
-    cout<<st.top()<<endl;
-    st.pop();
-    cout<<st.top()<<endl;
-    st.pop();
-    cout<<st.top()<<endl;
+
+    Stack<char> stt;
+    stt.push('a');
+    stt.push('g');
+    stt.push('u');
+    cout<<stt.top()<<endl;
+    stt.pop();
+    cout<<stt.top()<<endl;
+    stt.pop();
+    cout<<stt.top()<<endl;
+    stt.pop();
+    cout<<stt.top()<<endl;
+
+
 
     return 0;
 }
