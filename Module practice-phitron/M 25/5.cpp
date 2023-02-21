cf156fda052fa2062e9e513d9f63794f2db32e12
Junior Developer <lotifurshabbir54@gmail.com>
2023-02-21 13:43:38 +0600

    binary representation using queue

---
+++ Module practice-phitron/M 25/5.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+//TC 0(n)
+int main()
+{
+    int n;
+    cin >> n;
+    queue<string>q;
+    q.push("1");
+
+    for(int i = 1; i <= n; i++ )
+    {
+        cout << "Binary Representation of "<< i << ": " << q.front() << "\n";
+        string tmp = q.front();
+        q.pop();
+        q.push(tmp + "0");
+        q.push(tmp + "1");
+
+    }
+
+
+    return 0;
+}
